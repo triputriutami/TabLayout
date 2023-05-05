29dc80b0562b1dd94c0247596d52f8dbdd81d8cb
cicilianataliadesima <113664720+cicilianatalia@users.noreply.github.com>
2023-05-05 14:42:50 +0700

    tablayout

---
+++ app/src/main/java/com/triputriutami201103644/tablayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.triputriutami201103644.tablayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
